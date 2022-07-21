1c63912eaefcf1338d7c01eb8d4f0f12f9f555af
Jason Martinez <jasonmar08@gmail.com>
2022-07-20 20:58:39 -0400

    hw was complete

--- controllers/index.js
+++ controllers/index.js
@@ -1,72 +1,70 @@
-const plant = require("../models/plant");
-const Plant = require("../models/plant");
+const plant = require('../models/plant')
+const Plant = require('../models/plant')
 
 const createPlant = async (req, res) => {
   try {
-    const plant = await new Plant(req.body);
-    await plant.save();
-    return res.status(201).json({ plant });
+    const plant = await new Plant(req.body)
+    await plant.save()
+    return res.status(201).json({ plant })
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+    return res.status(500).json({ error: error.message })
   }
-};
+}
 const getAllPlants = async (req, res) => {
   try {
-    const plants = await Plant.find();
-    return res.status(200).json({ plants });
+    const plants = await Plant.find()
+    return res.status(200).json({ plants })
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+    return res.status(500).json({ error: error.message })
   }
-};
+}
 const getPlantsById = async (req, res) => {
   try {
-    const { id } = req.params;
-    const plant = await Plant.findById(id);
+    const { id } = req.params
+    const plant = await Plant.findById(id)
     if (plant) {
-      return res.status(200).json({ plant });
+      return res.status(200).json({ plant })
     }
-    return res
-      .status(404)
-      .send("Plant with the specified with ID does not exist");
+    return res.status(404).send('Plant with the specified ID does not exist')
   } catch (error) {
-    return res.status(500).send(error.message);
+    return res.status(500).send(error.message)
   }
-};
+}
 
 const updatePlant = async (req, res) => {
   try {
-    const { id } = req.params;
+    const { id } = req.params
     await Plant.findByIdAndUpdate(id, req.body, { new: true }, (err, plant) => {
       if (err) {
-        res.status(500).send(err);
+        res.status(500).send(err)
       }
       if (!plant) {
-        res.status(500).send("Plant not found!");
+        res.status(500).send('Plant not found!')
       }
-      return res.status(200).json(plant);
-    });
+      return res.status(200).json(plant)
+    })
   } catch (error) {
-    return res.status(500).send(error.message);
+    return res.status(500).send(error.message)
   }
-};
+}
 
 const deletePlant = async (req, res) => {
   try {
-    const { id } = req.params;
-    const deleted = await Plant.findByIdAndDelete(id);
+    const { id } = req.params
+    const deleted = await Plant.findByIdAndDelete(id)
     if (deleted) {
-      return res.status(200).send("Plant deleted");
+      return res.status(200).send('Plant deleted')
     }
-    throw new Error("Plant not found");
+    throw new Error('Plant not found')
   } catch (error) {
-    return res.status(500).send(error.message);
+    return res.status(500).send(error.message)
   }
-};
+}
 
 module.exports = {
   createPlant,
   getAllPlants,
   getPlantsById,
   updatePlant,
-  deletePlant,
-};
+  deletePlant
+}
